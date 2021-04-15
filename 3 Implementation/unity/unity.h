d35d402eea0ab3885122ee2f2974596392bfc983
Dipayan Saha <51865387+richik500@users.noreply.github.com>
2021-04-15 06:16:48 +0530

    commit

--- 3 Implementation/unity/unity.h
+++ 3 Implementation/unity/unity.h
@@ -1,5 +1,5 @@
 /* ==========================================
-    Unity Project - A Test Framework for C
+    Unity Project - A Test Framework for C 
     Copyright (c) 2007-19 Mike Karlesky, Mark VanderVoord, Greg Williams
     [Released under MIT License. Please refer to license.txt for details]
 ========================================== */
